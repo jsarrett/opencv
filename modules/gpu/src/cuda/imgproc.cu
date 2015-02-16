d0de329c4aee33973c52b36591f892b80fb02dea
contradict <contradict@gmail.com>
2015-02-16 09:51:07 -0800

    Add floating-point reprojectImageTo3D_gpu

--- modules/gpu/src/cuda/imgproc.cu
+++ modules/gpu/src/cuda/imgproc.cu
@@ -363,6 +363,8 @@ namespace cv { namespace gpu { namespace device
         template void reprojectImageTo3D_gpu<uchar, float4>(const PtrStepSzb disp, PtrStepSzb xyz, const float* q, cudaStream_t stream);
         template void reprojectImageTo3D_gpu<short, float3>(const PtrStepSzb disp, PtrStepSzb xyz, const float* q, cudaStream_t stream);
         template void reprojectImageTo3D_gpu<short, float4>(const PtrStepSzb disp, PtrStepSzb xyz, const float* q, cudaStream_t stream);
+        template void reprojectImageTo3D_gpu<float, float3>(const PtrStepSzb disp, PtrStepSzb xyz, const float* q, cudaStream_t stream);
+        template void reprojectImageTo3D_gpu<float, float4>(const PtrStepSzb disp, PtrStepSzb xyz, const float* q, cudaStream_t stream);
 
         /////////////////////////////////////////// Corner Harris /////////////////////////////////////////////////
 
